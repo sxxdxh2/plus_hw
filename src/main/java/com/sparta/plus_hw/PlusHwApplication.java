0892c28e47b9ae87b2f377f18b135abfed4badef
Dahee Son <sxxdxh2@gmail.com>
2023-12-17 15:08:26 +0900

    initial commit

---
+++ src/main/java/com/sparta/plus_hw/PlusHwApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.plus_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/plus_hw/PlusHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.plus_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
