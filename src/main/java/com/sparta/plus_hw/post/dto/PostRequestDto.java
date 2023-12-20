d0395ac800a23e1ddc80dac45e942e91370d6b5b
Dahee Son <sxxdxh2@gmail.com>
2023-12-20 16:23:55 +0900

    feat : 게시글 제목, 내용 글자수 제한

--- src/main/java/com/sparta/plus_hw/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta/plus_hw/post/dto/PostRequestDto.java
@@ -1,5 +1,6 @@
 package com.sparta.plus_hw.post.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SecondaryRow;
@@ -7,6 +8,8 @@ import org.hibernate.annotations.SecondaryRow;
 @Getter
 @Setter
 public class PostRequestDto {
+    @Size(max = 500)
     private String title;
+    @Size(max = 5000)
     private String contents;
 }
